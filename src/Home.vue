4a960c84b9862ca82ca5e1917b8d96a58ee83771
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-06 10:31:26 +0700

    Upload

---
+++ src/Home.vue
@@ -0,0 +1,16 @@
+<template><diV>
+<app-server-status v-for="Server in 5" :key="Server"></app-server-status>
+</div>
+</template>
+
+<script>
+import ServerStatus from './components/Server/ServerStatus.vue'
+export default {
+ components:{
+     'app-server-status': ServerStatus
+ }
+};
+</script>
+
+<style>
+</style>
